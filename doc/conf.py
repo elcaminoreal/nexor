f1108ba71c75353ca954569148569be2223c81c9
Moshe Zadka <moshez@zadka.club>
2023-12-08 16:03:41 +0000

    checkpoint

---
+++ doc/conf.py
@@ -0,0 +1,11 @@
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
+master_doc = 'index'
+project = 'nexor'
+copyright = 'Copyright (c) Moshe Zadka'
+author = 'Moshe Zadka'
+version = ''
+
+exclude_patterns = ['.ipynb_checkpoints/**']
