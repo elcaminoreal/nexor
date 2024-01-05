67db15d3e8482619cb08fa779fc8eba51cecd86d
Moshe Zadka <moshez@zadka.club>
2024-01-05 18:24:56 +0000

    checkpoint

--- src/nexor/cli.py
+++ src/nexor/cli.py
@@ -21,8 +21,13 @@ command = commandslib.make_command_register(_SUBCOMMANDS)
 def main(
     *, argv: Sequence[str], env: Mapping[str, str], run: Callable
 ) -> None:
+    def error(args):
+        parser.print_help()
+        raise SystemExit(1)
+    parser = argparse.ArgumentParser()
+    parser.set_defaults(__gather_command__=error)
     commandslib.run_maybe_dry(   
-        parser=commandslib.set_parser(collected=_SUBCOMMANDS.collect()),
+        parser=commandslib.set_parser(parser=parser, collected=_SUBCOMMANDS.collect()),
         argv=argv,
         env=env,
         sp_run=run,
