c6a2c81bc5bbe2d78f4a81436fb6e744dac1261a
Moshe Zadka <moshez@zadka.club>
2024-01-05 21:04:01 +0000

    checkpoint

--- src/nexor/cli.py
+++ src/nexor/cli.py
@@ -10,13 +10,16 @@ import toolz
 from . import __version__
 
 SUBCOMMANDS = gather.Collector()
-command = commandslib.make_command_register(SUBCOMMANDS)
+command = gather.commands.make_command_register(SUBCOMMANDS)
 
 
 @command()
 def version(args: argparse.ArgumentParser) -> None:
     print(__version__)
+    
+ns = argparse.Namespace()
 
+    
 main_command = toolz.compose(
     lambda _ignored: None,
     functools.partial(
@@ -26,4 +29,4 @@ main_command = toolz.compose(
     ),
 )
 
-sub_command = functools.partial(main_command, init_globals=dict(IS_SUBCOMMAND=True))
+ns.sub_command = functools.partial(main_command, init_globals=dict(IS_SUBCOMMAND=True))
