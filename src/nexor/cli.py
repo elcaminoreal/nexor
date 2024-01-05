799f2f72211bb84019ad65a06a118e0dc3ae8288
Moshe Zadka <moshez@zadka.club>
2024-01-05 18:31:09 +0000

    checkpoint

--- src/nexor/cli.py
+++ src/nexor/cli.py
@@ -1,15 +1,8 @@
 import argparse
-import functools
 import logging
-import os
-import pathlib
-import textwrap
 from typing import Sequence, Mapping, Callable
-import subprocess
-import sys
 
 import gather
-from gather.commands import add_argument
 from gather import commands as commandslib
 
 LOGGER = logging.getLogger(__name__)
@@ -17,16 +10,16 @@ LOGGER = logging.getLogger(__name__)
 _SUBCOMMANDS = gather.Collector()
 
 command = commandslib.make_command_register(_SUBCOMMANDS)
-    
-def main(
-    *, argv: Sequence[str], env: Mapping[str, str], run: Callable
-) -> None:
+
+
+def main(*, argv: Sequence[str], env: Mapping[str, str], run: Callable) -> None:
     def error(args):
         parser.print_help()
         raise SystemExit(1)
+
     parser = argparse.ArgumentParser()
     parser.set_defaults(__gather_command__=error)
-    commandslib.run_maybe_dry(   
+    commandslib.run_maybe_dry(
         parser=commandslib.set_parser(parser=parser, collected=_SUBCOMMANDS.collect()),
         argv=argv,
         env=env,
--- src/nexor/tests/test_cli.py
+++ src/nexor/tests/test_cli.py
@@ -1,23 +1,24 @@
 import contextlib
 import unittest
-from hamcrest import assert_that, all_of, not_, calling, contains_string, has_entry, has_key, raises
+from hamcrest import (
+    assert_that,
+    calling,
+    contains_string,
+    raises,
+)
 from unittest import mock
-import pathlib
-import os
-import tempfile
-import subprocess
 from io import StringIO
 
 from .. import cli
 
-class TestMain(unittest.TestCase): # pragma: no cover
+
+class TestMain(unittest.TestCase):  # pragma: no cover
     def test_help(self):
         with contextlib.ExitStack() as stack:
             stdout = stack.enter_context(mock.patch("sys.stdout", new=StringIO()))
-            stderr = stack.enter_context(mock.patch("sys.stderr", new=StringIO()))
             assert_that(
                 calling(cli.main).with_args(
-                    run=lambda:None,
+                    run=lambda: None,
                     argv=["nexor"],
                     env={},
                 ),
