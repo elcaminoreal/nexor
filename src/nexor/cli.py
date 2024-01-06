ce6952be140147b3ace594e83d92cc64f158b512
Moshe Zadka <moshez@zadka.club>
2024-01-06 00:35:55 +0000

    checkpoint

--- src/nexor/cli.py
+++ src/nexor/cli.py
@@ -5,6 +5,6 @@ from . import __version__
 ENTRY_DATA = entry.EntryData.create("nexor")
 command = ENTRY_DATA.register
 
-@command()
-def version(args: argparse.ArgumentParser) -> None:
+@command(name="version")
+def _version(args: argparse.ArgumentParser) -> None:
     print(__version__)
