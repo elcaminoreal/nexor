370008a0243bb42ce0a839cf6e6ef3456c4234fe
Moshe Zadka <moshez@zadka.club>
2024-01-06 00:15:34 +0000

    checkpoint

--- src/nexor/__main__.py
+++ src/nexor/__main__.py
@@ -1,22 +1,8 @@
-import os
-import logging
-import subprocess
-import sys
-
-from gather import commands as commandslib
+from gather import entry
 
 from . import cli
 
-if __name__ != "__main__":
-    raise ImportError("module cannot be imported", __name__)
-
-logging.basicConfig(
-    format="%(asctime)s:%(levelname)s:%(name)s:%(message)s",
-    level=logging.INFO,
-)
-
-commandslib.run_maybe_dry(
-    parser=commandslib.set_parser(collected=cli.SUBCOMMANDS.collect()),
-    is_subcommand=globals().get("IS_SUBCOMMAND", False),
-    prefix="nexor",
+entry.dunder_main(
+    globals_dct=globals(),
+    command_data=cli.ENTRY_DATA,
 )
--- src/nexor/cli.py
+++ src/nexor/cli.py
@@ -4,29 +4,13 @@ import logging
 from typing import Sequence, Mapping, Callable
 import runpy
 
-import gather.commands
-import toolz
+from gather import entry
 
 from . import __version__
-
-SUBCOMMANDS = gather.Collector()
-command = gather.commands.make_command_register(SUBCOMMANDS)
-
+    
+ENTRY_DATA = entry.EntryData.create("nexor")
+command = ENTRY_DATA.register
 
 @command()
 def version(args: argparse.ArgumentParser) -> None:
     print(__version__)
-    
-ns = argparse.Namespace()
-
-    
-main_command = toolz.compose(
-    lambda _ignored: None,
-    functools.partial(
-        runpy.run_module,
-        "nexor",
-        run_name="__main__",
-    ),
-)
-
-ns.sub_command = functools.partial(main_command, init_globals=dict(IS_SUBCOMMAND=True))
